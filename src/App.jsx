ae930c1c58064cca4d8364be4fab30ae838d86bf
Coding With Enjoy <codingwithenjoy@gmail.com>
2023-03-16 14:41:57 +0330

    First Deployment

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Hero from "./components/Hero";
+import Navbar from "./Layouts/Navbar";
+import Skills from "./components/Skills";
+import Service from "./components/Services";
+import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
+import Hireme from "./components/Hireme";
+import Contact from "./components/Contact";
+import { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const App = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1800,
+      offset: 100,
+      disable: "mobile",
+    });
+  }, []);
+  return (
+    <div className="">
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Service />
+      <Projects />
+      <Testimonials />
+      <Hireme />
+      <Contact />
+      <footer className="p-3 text-center">
+        <h6 className="mb-3">Amirmasoud Abedi</h6>
+        <p>Coding With Enjoy All © Copy Right Reserved 2023</p>
+      </footer>
+    </div>
+  );
+};
+
+export default App;
